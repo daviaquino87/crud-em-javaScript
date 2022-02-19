6ea830d45b98ce74b9011eccde7d484d26bd1b97
daviaquino87 <davi26031@gmail.com>
2022-02-19 19:58:39 -0300

    create finalizado

--- js/main.js
+++ js/main.js
@@ -7,27 +7,25 @@ const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
 const tempClient = {
-    nome:"maria",
-    email:"davi26031@gmail.com2",
+    nome:"teste",
+    email:"teste@gmail.com",
     celular:"88998421205",
-    cidade:"pereiro2"
+    cidade:"pereiro"
 }
-
-
 
 //CRUD - CREATE - READ - UPDATE - DELETE
 
+////////////////////////////////////////////////////////////////////
 //CREATE
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (dbClient) =>  localStorage.setItem("db_client",JSON.stringify(dbClient));
 
-//minha variavel createCliente recebe um cliente e passa pra uma arrow function que manda para o localstorage
-//o local storage usa a função setitem para pega o valor e salvar no local storage no formato de chave e valor
-//o json.stringfy esta servindo para pegar nosso objeto que esta vindo em json e transformalo em string 
 const createClient = (client) => {
-    const db_client = JSON.parse(localStorage.getItem("db_client"));
-    console.log(db_client);
-    db_client.push(client)
-    localStorage.setItem("db_client:",JSON.stringify(db_client));
+    const dbClient = getLocalStorage();
+    dbClient.push(client);
+    setLocalStorage(dbClient);
 }
+////////////////////////////////////////////////////////////////////
 
 
 //eventos
