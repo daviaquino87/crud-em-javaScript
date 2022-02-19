f9fda224ff436ba03d72615e3ebd2887db794d56
daviaquino87 <davi26031@gmail.com>
2022-02-19 20:07:07 -0300

    read finalizado

--- js/main.js
+++ js/main.js
@@ -16,6 +16,15 @@ const tempClient = {
 //CRUD - CREATE - READ - UPDATE - DELETE
 
 ////////////////////////////////////////////////////////////////////
+//READ
+const readClient = () =>  getLocalStorage();
+////////////////////////////////////////////////////////////////////
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
 //CREATE
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
 const setLocalStorage = (dbClient) =>  localStorage.setItem("db_client",JSON.stringify(dbClient));
